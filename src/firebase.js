2ee6166a11523abbc574c0e00999af847321cdda
althof <althoframdhan3@gmail.com>
2020-08-13 07:17:35 +0700

    🎈 using env react

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
-import {API_KEY, STORAGE, DATABASE} from './notes.js'
 
 
 const firebaseApp = firebase.initializeApp({
 
-    apiKey: API_KEY,
+    apiKey: process.env.API_KEY,
     authDomain: "messenger-app-73ccd.firebaseapp.com",
-    databaseURL: DATABASE,
+    databaseURL: process.env.DATABASE,
     projectId: "messenger-app-73ccd",
-    storageBucket: STORAGE,
+    storageBucket: process.env.STORAGE,
     messagingSenderId: "132091890762",
     appId: "1:132091890762:web:9fba75df8ac5ef5dd5aab3",
     measurementId: "G-4MB497LHT2"
